4bf874df08c221bbe473d85d50ffb7b02c2563c8
Jason Cantor <46542133+jasoncantor@users.noreply.github.com>
2024-04-19 10:41:54 -0700

    Update 20240108234540_setup.sql

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,8 +50,8 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'http://supabase_kong_chatbotui:8000';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU'; -- full access needed for http request to storage
+  project_url TEXT := 'https://smamtwdjxecuelzdyqck.supabase.co';
+  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtYW10d2RqeGVjdWVsemR5cWNrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMzIwMjk4OSwiZXhwIjoyMDI4Nzc4OTg5fQ.mNC9UxySkeSkD8jny9ISnkIMeirZbrdwZHqUTeYEZMU'; -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
